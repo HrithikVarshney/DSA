3267c08373a16a0143c39268e01e0ec773f022b6
hrithik varshney <hrithikvarshney19@gmail.com>
2024-07-14 00:56:57 +0530

    Added 5 codes

--- src/Arrays/leftRotateByDPlaces.java
+++ src/Arrays/leftRotateByDPlaces.java
@@ -1,4 +1,20 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class leftRotateByDPlaces {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+        int dPlaces = 3;
+        ArrayList<Integer> tempArr = funLeftRotate(arr , dPlaces);
+        System.out.println(tempArr.toString());
+    }
+
+    static ArrayList<Integer> funLeftRotate(ArrayList<Integer> arr , int d){
+
+
+
+        return arr;
+    }
 }
--- src/Arrays/removeDuplicates.java
+++ src/Arrays/removeDuplicates.java
@@ -1,4 +1,23 @@
 package Arrays;
 
+import java.util.*;
+
 public class removeDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(6,5,57,6,1,2,3,4,5,5,4,3,2,1,5,3,2,1,3));
+        //ArrayList<Integer> tempArr =
+        fun1RemoveDuplicates(arr);
+       // System.out.println(tempArr.toString());
+    }
+
+    static void fun1RemoveDuplicates (ArrayList<Integer> arr){
+        Set<Integer> ss = new HashSet<>();
+        for(int i = 0 ; i<arr.size() ; i++){
+            ss.add(arr.get(i));
+        }
+
+        for(Integer ele : ss){
+            System.out.print(ele+ " " );
+        }
+    }
 }
